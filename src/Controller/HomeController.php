588f6f99152dc7f1d19e8bbf99f333b61ae6771d
Langlet Gaëtan <gtn.langlet@gmail.com>
2023-06-27 19:44:03 +0200

    init front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Figure;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $figures = $entityManager->getRepository(Figure::class)->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'tricks' => $figures
         ]);
     }
+
 }
